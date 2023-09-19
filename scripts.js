573aa1ad2883079990b12e3f3923cfdd3b9d1765
Thalyssonh3 <thalysson.h3silva@gmail.com>
2023-09-19 01:03:27 -0300

    Atualização do site

--- scripts.js
+++ scripts.js
@@ -3,3 +3,8 @@ function trocaCor(cor){
     circle.style.background = cor
 }
 
+function trocaImage(imagem){
+    let imgIphone = document.querySelector(".image-iphone")
+    imgIphone.src = imagem
+
+}
